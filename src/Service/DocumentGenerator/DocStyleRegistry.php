f152b54d4fd2559a81ae1b4641274ee25baca6a1
a.ulyanov <a.ulyanov@zaymigo.ru>
2026-06-30 00:34:39 +0500

    стили абзацев

--- src/Service/DocumentGenerator/DocStyleRegistry.php
+++ src/Service/DocumentGenerator/DocStyleRegistry.php
@@ -153,7 +153,7 @@ final class DocStyleRegistry
             'indentation' => [
                 'left' => 0,
                 'right' => 0,
-                'firstLine' => Converter::cmToTwip(1.25),
+                'firstLine' => (int) round(Converter::cmToTwip(1.25)),
                 'hanging' => 0,
             ],
             'spaceAfter' => 0,
